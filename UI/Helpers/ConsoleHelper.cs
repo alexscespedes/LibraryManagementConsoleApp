6849f42fa86009eb7f72d1e9f13527a8c17957b3
alexscespedes <alexandersenc99@gmail.com>
2025-05-12 18:50:28 -0400

    Add view all books & valid isbn code helpers

--- UI/Helpers/ConsoleHelper.cs
+++ UI/Helpers/ConsoleHelper.cs
@@ -1,6 +1,26 @@
+using System.Text.RegularExpressions;
+
 namespace LibraryManagement;
 
 public class ConsoleHelper 
 {
-    
+    public void PrintBook(Book book) 
+    {
+        string status = book.IsAvailable ? "Available" : "Not Available";
+        Console.WriteLine($" ISBN: {book.ISBN} | Title: {book.Title} | Author: {book.Author} | Genre: {book.GenreBook} | Status: {status} ");
+    }
+
+    public static bool IsValidISBNCode(string str) 
+    {
+        string strRegex = @"^(?=(?:[^0:9]*[0:9]){10}(?:(?:[^0:9]*[0:9]){3})?$)[\d-]+$";
+        Regex re = new Regex(strRegex);
+        if (re.IsMatch(str))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
 }
